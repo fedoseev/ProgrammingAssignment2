dab920d48b37aef6da0ec483bf4662292b3a1a37
Alex Fedoseev <alex@technextoor.us>
2015-01-25 18:21:23 -0500

    Wrote function description

--- cachematrix.R
+++ cachematrix.R
@@ -5,14 +5,23 @@
 
 
 
-## Write a short comment describing this function
+## makeCacheMatrix function 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y){
+       x <<- y
+       m <<- NULL
+  }
+get <- function()x
+setInverse <-function()
 }
 
 
-## Write a short comment describing this function
+## cacheSolve will return the inverse of the matrix object. 
+## The special feature of this function is that it checks if there is alreay a value in the internal cache and the original matrix has not changed.
+## If there is a value in the cache, the function will return the cached value. 
+## If there is none, the fuction will compute the inverse, store its value and then return the computed value.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
