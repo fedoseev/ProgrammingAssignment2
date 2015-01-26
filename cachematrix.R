f7ca0565fe4dd83bd1094a10a5b8b4dfaf30e181
Alex Fedoseev <alex@technextoor.us>
2015-01-25 19:18:30 -0500

    Final version
    Finial version of the project,
    added the code for cacheSolve  function and testing scenario in the
    comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,25 @@
 ##  Class: "R Programming" at Coursera.com
 ##  Assignment 2 
 ##  The functions below are to illustrate the consept of caching using R scoping properties.
+##
+##  Testing  scenario:
+##x = rbind(c(4, 7), c(2, 6)) 
+##y = makeCacheMatrix(x)
+##  Run cacheSolve(y). This is an initial run, so the return value of a function must be computed first
+##cacheSolve(y)
+##  The second run. This time the value is extracted from the cache
+#cacheSolve(y)
 
-
+##   [,1] [,2]
+##  [1,]  0.6 -0.7
+##  [2,] -0.2  0.4
 
 
 ## makeCacheMatrix function 
-## This function creates a matrix a list that contains the function to get and set the values of the matrix
-## and get and set the values of its inverse
-##
-##   
+## This function creates a matrix a list that contains the function: 
+## 1.To get and set the values of the matrix
+## 2. To get and set the values of the matrix inverse
+
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL # assigning initial value
@@ -33,8 +43,19 @@ list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 ## If there is a value in the cache, the function will return the cached value. 
 ## If there is none, the fuction will compute the inverse, store its value and then return the computed value.
 
+
+## The Inverse of a Matrix is the same idea as the reciprocal of a number:
+
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-  
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("Getting cached data...")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
   
 }
